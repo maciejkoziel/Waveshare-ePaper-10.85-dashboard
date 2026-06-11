163c906e7531af9c883cca472b3c0182d5c86088
Maciej Koziel <maciej.koziel@censhare.com>
2026-06-11 13:36:18 +0200

    style: black background for left-rail next event widget

--- main.py
+++ main.py
@@ -745,11 +745,11 @@ def draw_next_event_card(draw, fonts, x, w, top, h, ev, now_utc, today_date):
 
 
 def draw_next_event_rail(draw, fonts, x, y, w, ev, now_utc, today_date):
-    draw.line((x + 8, y, x + w - 8, y), fill='black', width=1)
+    draw.rectangle((x, y, x + w, y + 108), fill='black')
     soon = (not ev.get('allday', True) and
             0 <= (ev['dt'] - now_utc).total_seconds() <= 10800)
-    tc = 'red' if soon else 'black'
-    draw.text((x + 12, y + 8), STRINGS.get('next_title', 'NEXT'), font=fonts['b22'], fill='black')
+    tc = 'red' if soon else 'white'
+    draw.text((x + 12, y + 8), STRINGS.get('next_title', 'NEXT'), font=fonts['b22'], fill='yellow')
     if ev.get('allday'):
         delta = (ev['event_date'] - today_date).days
         if delta == 0:
@@ -776,9 +776,9 @@ def draw_next_event_rail(draw, fonts, x, y, w, ev, now_utc, today_date):
                 countdown = STRINGS.get('next_in_m', 'in {minutes}m').format(minutes=minutes)
     if countdown:
         draw.text((x + w - 12 - int(fonts['r20'].getlength(countdown)), y + 10),
-                  countdown, font=fonts['r20'], fill=tc)
-    sq_color = 'black' if ev['calendar'] == 'personal' else 'yellow'
-    draw.rectangle([x + 12, y + 40, x + 26, y + 54], fill=sq_color, outline='black')
+                  countdown, font=fonts['r20'], fill='yellow')
+    sq_color = 'white' if ev['calendar'] == 'personal' else 'yellow'
+    draw.rectangle([x + 12, y + 40, x + 26, y + 54], fill=sq_color, outline='white')
     draw.text((x + 34, y + 36), big, font=fonts['b36'], fill=tc)
     draw.text((x + 12, y + 82), fit_text(ev['title'], fonts['r22'], w - 24), font=fonts['r22'], fill=tc)
 
